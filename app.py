a87631a3ae5d479511123273a2b55c53a94984d9
Flora2020 <rossignols1989@gmail.com>
2021-12-05 20:16:03 +0800

    refactor

--- app.py
+++ app.py
@@ -1,69 +1,69 @@
-def generate_integers():
-    # validate user input
-    error_message = {
-        'not_integers': 'Sorry, Please input integer only.',
-        'not_increasing': 'Sorry, the sequence of integers is not increasing.',
-    }
+class Find_pattern:
+    def __init__(self) -> None:
+        self.error_message = {
+            'not_integers': 'Error, please enter integer only.',
+            'not_increasing': 'Error, please enter increasing sequence.'
+        }
+        self.origional_integer_list = []
+        self.number_of_generated_integers = 10
+        self.generated_integers = []
+        self.result_message = f'The next {self.number_of_generated_integers} integers:\n'
+        self.difference = []
+        self.quotient_remainder_pairs = []
 
-    integers = input('Please enter increasing integers separated by spaces:\n')
-    integer_list = integers.split()
+    def update_initial_setting(self, input: list) -> None:
+        try:
+            self.origional_integer_list = [int(item) for item in input]
+        except:
+            raise Exception(self.error_message['not_integers'])
 
-    try:
-        integer_list = [int(item) for item in integer_list]
-    except:
-        raise Exception(error_message['not_integers'])
+        for i in range(1, len(self.origional_integer_list)):
+            prev = self.origional_integer_list[i - 1]
+            current = self.origional_integer_list[i]
 
-    # init
-    number_of_generated_integers = 10
-    generated_integers = []
-    result_message = f'the next {number_of_generated_integers} integers:\n'
+            if current < prev:
+                raise Exception(self.error_message['not_increasing'])
+            self.difference.append(current - prev)
+            self.quotient_remainder_pairs.append(divmod(current, prev))
 
-    # find the pattern
-    difference = []
-    quotient_remainder_pairs = []
+    def generate_integers(self, generate_next_int: callable) -> None:
+        prev_number = self.origional_integer_list[-1]
+        self.generated_integers.append(generate_next_int(prev_number))
 
-    for i in range(1, len(integer_list)):
-        prev = integer_list[i - 1]
-        current = integer_list[i]
+        for i in range(self.number_of_generated_integers - 1):
+            prev_number = self.generated_integers[-1]
+            self.generated_integers.append(generate_next_int(prev_number))
 
-        if current < prev:
-            raise Exception(error_message['not_increasing'])
+    def find_pattern(self) -> str:
+        try:
+            integers_string = input(
+                'Please enter increasing integers separated by spaces:\n'
+            )
 
-        difference.append(current - prev)
-        quotient_remainder_pairs.append(divmod(current, prev))
+            self.update_initial_setting(integers_string.split())
+            generate_next_int = None
 
-    # pattern: arithmetic sequence
-    if len(set(difference)) == 1:
-        def append_next_number(prev):
-            generated_integers.append((prev + difference[0]))
-            return
+            # pattern: arithmetic sequence
+            if len(set(self.difference)) == 1:
+                def generate_next_int(prev): return prev + self.difference[0]
 
-        append_next_number(integer_list[-1])
+            # pattern: geometric-like sequence
+            elif len(set(self.quotient_remainder_pairs)) == 1:
+                q, r = self.quotient_remainder_pairs[0]
+                def generate_next_int(prev): return prev * q + r
 
-        for i in range(number_of_generated_integers - 1):
-            append_next_number(generated_integers[-1])
+            if generate_next_int is None:
+                self.result_message = 'Sorry, I cannot recognize the pattern of the sequence.'
+            else:
+                self.generate_integers(generate_next_int)
+                self.result_message += ' '.join([str(num)
+                                                for num in self.generated_integers])
 
-    # pattern: geometric sequence like
-    elif len(set(quotient_remainder_pairs)) == 1:
-        q, r = quotient_remainder_pairs[0]
+            return self.result_message
 
-        def append_next_number(prev):
-            generated_integers.append(prev * q + r)
-            return
-
-        append_next_number(integer_list[-1])
-
-        for i in range(number_of_generated_integers - 1):
-            append_next_number(generated_integers[-1])
-
-    if generated_integers:
-        result_message += ' '.join([str(num) for num in generated_integers])
-    else:
-        result_message = 'Sorry, I cannot recognize the pattern of the sequence.'
-    return result_message
+        except Exception as e:
+            return str(e)
 
 
-try:
-    print(generate_integers())
-except Exception as e:
-    print(e)
+find_pattern = Find_pattern()
+print(find_pattern.find_pattern())
